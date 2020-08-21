1029d80fe0c0bd4dd278c132d7874a35db23c38c
Joseph Mills <josephmills@mills-it.co.uk>
2020-08-21 15:31:31 +0100

    change to ng-select for search

--- src/app/search/search-box/search-box.component.ts
+++ src/app/search/search-box/search-box.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { debounceTime, tap, switchMap, finalize, filter } from 'rxjs/operators';
+import {
+  debounceTime,
+  tap,
+  switchMap,
+  finalize,
+  filter,
+  distinctUntilChanged,
+  catchError,
+} from 'rxjs/operators';
 import { SearchService } from '../search.service';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Observable, concat, of, Subject } from 'rxjs';
 export interface SearchItem {
   id: number;
   name: string;
@@ -20,12 +29,43 @@ export interface SearchItem {
   styleUrls: ['./search-box.component.scss'],
 })
 export class SearchBoxComponent implements OnInit {
+  // cities = [
+  //   {
+  //     id: 1,
+  //     name: 'Vilnius',
+  //     avatar:
+  //       '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Kaunas',
+  //     avatar:
+  //       '//www.gravatar.com/avatar/ddac2aa63ce82315b513be9dc93336e5?d=retro&r=g&s=15',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Pavilnys',
+  //     avatar:
+  //       '//www.gravatar.com/avatar/6acb7abf486516ab7fb0a6efa372042b?d=retro&r=g&s=15',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Siauliai',
+  //     avatar:
+  //       '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x',
+  //   },
+  // ];
+
   @Output() searchItemSelected = new EventEmitter<SearchItem>();
   form: FormGroup;
   filteredResults: SearchItem[];
+  searchItems$: Observable<SearchItem[]>;
+  searchInput$ = new Subject<string>();
   searchItems: SearchItem[] = [];
   isLoading = false;
   errorMsg: string;
+  selectedCity;
+  searchLoading = false;
   get search() {
     return this.form.get('search');
   }
@@ -38,32 +78,49 @@ export class SearchBoxComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.search.valueChanges
-      .pipe(
-        debounceTime(500),
-        tap(() => {
-          this.errorMsg = '';
-          this.filteredResults = [];
-          this.isLoading = true;
-        }),
-        filter((x) => !!x),
-        switchMap((value) =>
-          this.searchService.get(value).pipe(
-            finalize(() => {
-              this.isLoading = false;
-            })
+    // this.search.valueChanges
+    //   .pipe(
+    //     debounceTime(500),
+    //     tap(() => {
+    //       this.errorMsg = '';
+    //       this.filteredResults = [];
+    //       this.isLoading = true;
+    //     }),
+    //     filter((x) => !!x),
+    //     switchMap((value) =>
+    //       this.searchService.get(value).pipe(
+    //         finalize(() => {
+    //           this.isLoading = false;
+    //         })
+    //       )
+    //     )
+    //   )
+    //   .subscribe((data) => {
+    //     if (data == undefined) {
+    //       // this.errorMsg = data.error;
+    //       this.filteredResults = [];
+    //     } else {
+    //       this.errorMsg = '';
+    //       this.filteredResults = [...this.filteredResults, ...data];
+    //     }
+    //   });
+    this.loadSearch();
+  }
+
+  private loadSearch() {
+    this.searchItems$ = concat(
+      of([]), // default items
+      this.searchInput$.pipe(
+        distinctUntilChanged(),
+        tap(() => (this.searchLoading = true)),
+        switchMap((term) =>
+          this.searchService.get(term).pipe(
+            catchError(() => of([])), // empty list on error
+            tap(() => (this.searchLoading = false))
           )
         )
       )
-      .subscribe((data) => {
-        if (data == undefined) {
-          // this.errorMsg = data.error;
-          this.filteredResults = [];
-        } else {
-          this.errorMsg = '';
-          this.filteredResults = data;
-        }
-      });
+    );
   }
   buildForm() {
     this.form = this.fb.group({
@@ -77,11 +134,10 @@ export class SearchBoxComponent implements OnInit {
       queryParamsHandling: 'merge',
     });
   }
-  searchItemSelect(selection: MatAutocompleteSelectedEvent) {
-
-    const selectedItem = this.filteredResults.find(
-      (x) => x.id === +selection.option.value
-    );
-    this.searchItemSelected.emit(selectedItem);
+  searchItemSelect(selection: SearchItem) {
+    this.searchInput$.next('');
+    if (selection) {
+      this.searchItemSelected.emit(selection);
+    }
   }
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,18 +3,23 @@ import { CommonModule } from '@angular/common';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { SharedModule } from '../shared/shared.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 @NgModule({
   declarations: [SearchBoxComponent],
   exports: [SearchBoxComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    NgSelectModule,
+    FormsModule,
     SharedModule,
     MatFormFieldModule,
     MatInputModule,
+    NgOptionHighlightModule,
     MatAutocompleteModule,
   ],
 })
